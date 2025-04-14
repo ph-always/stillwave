4719e8d15fd17eca6727f6b5a5bcc010d49706ee
Phoenix by ruon.ai <ph@re.works>
2025-04-14 09:11:06 +0900

    A

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import author from './potstType'
 
 export const schemaTypes = [post, author, category, blockContent]
